dc1a84214353a7df140b7a110d0d76b6bbb0e310
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-06-27 22:09:50 +0000

    Hotfix 1  fix image error prod (#4)
    * fix path for icons
    
    * Add icon types

--- data/menu.ts
+++ data/menu.ts
@@ -1,21 +1,22 @@
 import { Menu } from "../types/types"
-
+import CovidLogo from '../public/assets/images/covid.png'
+import HivLogo from '../public/assets/images/AIDS Ribbonhiv.png'
 export const menuItems:Menu[] = [
     {
        id: 1,
-       icon: '/../public/assets/images/covid.png',
+       icon: CovidLogo,
        title: 'COVID-19 vaccination RRI',
        description: 'View Covid-19 Statically data'
     },
     { 
         id: 2,
-        icon: '/../public/assets/images/AIDS Ribbonhiv.png',
+        icon: HivLogo,
         title: 'HIV services, Care & Treatment',
         description: 'View HIV & AIDS Statically data'
      },
      {
         id: 3,
-        icon: '/../public/assets/images/AIDS Ribbonhiv.png',
+        icon: HivLogo,
         title: '',
         description: 'View Malaria Statically data'
      }, 
--- types/types.d.ts
+++ types/types.d.ts
@@ -1,6 +1,8 @@
+import { StaticImageData } from "next/image"
+
 export interface Menu{
     id: number
-    icon: string
+    icon: StaticImageData
     title: string
     description: string
    
