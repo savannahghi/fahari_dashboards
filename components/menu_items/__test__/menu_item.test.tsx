9b0721061e2067596f9036c571b93e9284d82aa0
Martin <martinmwenda026@gmail.com>
2022-07-04 22:31:00 +0300

    test: menu-items-component

--- components/menu_items/__test__/menu_item.test.tsx
+++ components/menu_items/__test__/menu_item.test.tsx
@@ -1,12 +1,12 @@
-import { screen, render, fireEvent } from '@testing-library/react'
-import CovidLogo from '../../../public/assets/images/covid.png'
-import MenuItems from '../menu_items'
+import { screen, render, fireEvent } from "@testing-library/react"
+import CovidLogo from "../../../public/assets/images/covid.png"
+import MenuItems from "../menu_items"
 
-describe('Test Menu Item Component', () => {
+describe("Test Menu Item Component", () => {
   const MockMenuItem = () => {
     return (
       <>
-        <MenuItems id={0} icon={CovidLogo} title={''} description={''} />
+        <MenuItems id={0} icon={CovidLogo} title={""} description={""} />
       </>
     )
   }
@@ -15,33 +15,33 @@ describe('Test Menu Item Component', () => {
     render(<MockMenuItem />)
   })
 
-  test('Componet should be visible', () => {
+  test("Componet should be visible", () => {
     expect(MockMenuItem).toBeVisible
   })
 
-  test('should render a visible image', () => {
-    const imageElement = screen.getByRole('img')
+  test("should render a visible image", () => {
+    const imageElement = screen.getByRole("img")
 
     expect(imageElement).toBeInTheDocument
   })
 
-  test('should  render a heading element', () => {
-    const headingElement = screen.getByRole('heading')
+  test("should  render a heading element", () => {
+    const headingElement = screen.getByRole("heading")
 
     expect(headingElement).toBeVisible
   })
 
-  test('should render a paragraph element', () => {
-    const paragraphElement = screen.getByTestId('paragraph')
+  test("should render a paragraph element", () => {
+    const paragraphElement = screen.getByTestId("paragraph")
 
     expect(paragraphElement).toBeVisible
   })
 
-  test('article element should be clickable', () => {
-    const articleElement = screen.getByTestId('article')
+  test("should navigate", () => {
+    const clickableElement = screen.getByTestId("click")
 
-    fireEvent.click(articleElement)
+    fireEvent.click(clickableElement)
 
-    expect(articleElement).not.toBeInTheDocument
+    expect(clickableElement).not.toBeVisible
   })
 })
--- components/menu_items/menu_items.tsx
+++ components/menu_items/menu_items.tsx
@@ -1,28 +1,33 @@
-import { Menu } from "../../types/types";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import { Menu } from "../../types/types"
+import Image from "next/image"
+import { useRouter } from "next/router"
 
-const MenuItems = (props: Menu) =>{
+const MenuItems = (props: Menu) => {
+  const router = useRouter()
 
-    const router = useRouter();
-
-    const navigateToPage = () =>{
-       if(String(props.id).includes("1")){
-          router.push('covid');     
-       } 
-
-       if(String(props.id).includes("2")){
-         router.push('hts');
-       }
+  const navigateToPage = () => {
+    if (String(props.id).includes("1")) {
+      router.push("covid")
     }
 
-    return (
-        <article className="text-center  w-80 h-44 hover:shadow-2xl hover:rounded-md cursor-pointer hover:scale-105" data-testid="article" onClick={navigateToPage}> 
-                   <Image src={props.icon}  width ={100} height ={100} alt="menu_logo" />
-            <h1 className="font-semibold">{props.title}</h1>
-            <p className=" font-light" data-testid="paragraph">{props.description}</p>
-        </article>
-    );
+    if (String(props.id).includes("2")) {
+      router.push("hts")
+    }
+  }
+
+  return (
+    <section
+      className="text-center  w-80 h-44 hover:shadow-2xl hover:rounded-md cursor-pointer hover:scale-105"
+      onClick={navigateToPage}
+      data-testid="click"
+    >
+      <Image src={props.icon} width={100} height={100} alt="menu_logo" />
+      <h1 className="font-semibold">{props.title}</h1>
+      <p className=" font-light" data-testid="paragraph">
+        {props.description}
+      </p>
+    </section>
+  )
 }
 
-export default MenuItems;
+export default MenuItems
