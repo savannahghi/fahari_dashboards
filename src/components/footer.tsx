86c8722820cfca09605e82917072a4887602145c
gichuivictor <141823022+gichuivictor@users.noreply.github.com>
2024-03-19 15:52:20 +0300

    update copyright text ui(#19) (#19)
    - Update copyright text to SGHI
    - Update footer component text
    - Update contributors in package.json

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
       <div className="mx-auto overflow-hidden px-6 py-4 lg:px-8">
         <p className="mt-4 text-center text-xs leading-5 text-gray-500">
           &copy;
-          {` ${currentYear} Savannah Informatics Limited, All rights reserved.`}
+          {` ${currentYear} Savannah Global Health Institute, All rights reserved.`}
         </p>
       </div>
     </footer>
